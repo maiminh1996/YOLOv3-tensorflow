0e274df3988a22afa4e83568d5a57015c4a49e64
MAI <nguyen.mai@insa-cvl.fr>
2018-07-20 11:53:14 +0200

    fin

--- utils/yolo_utils.py
+++ utils/yolo_utils.py
@@ -1,13 +1,8 @@
-import colorsys
 import random
 import cv2
-from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
 import numpy as np
-import tensorflow as tf
 from PIL import Image
 import os
-import tqdm
-
 
 
 def read_classes(classes_path):
@@ -22,134 +17,6 @@ def read_anchors(anchors_path):
         anchors = [float(x) for x in anchors.split(',')]
         anchors = np.array(anchors).reshape(-1, 2)
     return anchors
-
-def rand(a=0, b=1):
-    return np.random.rand()*(b-a) + a
-
-# def get_training_data(annotation_path, data_path, input_shape, anchors, num_classes,load_previous=True, max_boxes=20, jitter=.3, hue=.1, sat=1.5, val=1.5, proc_img=True, random=False):
-#     '''random preprocessing for real-time data augmentation'''
-#     if load_previous == True and os.path.isfile(data_path):
-#         data = np.load(data_path)
-#         print('Loading training data from ' + data_path)
-#         # return data['image_data'], data['box_data'], data['image_shape']
-#         return data['image_data'], data['box_data'], data['image_shape'], [data['y_true0'], data['y_true1'], data['y_true2']]
-#         # return data['image_data'], data['box_data'], data['image_shape'], [data['y_true']]
-#     image_data = []
-#     box_data = []
-#     image_shape = []
-#     with open(annotation_path) as f:
-#         GG = f.readlines()
-#         np.random.shuffle(GG)
-#         for line in GG:
-#             line = line.split()
-#             image = Image.open(line[0])
-#             import matplotlib.pyplot as plt
-#             # plt.imshow(np.array(image))
-#             # plt.show()
-#             iw, ih = image.size
-#             h, w = input_shape
-#             box = np.array([np.array(list(map(int,box.split(',')))) for box in line[1:]])
-#
-#             if not random:
-#                 # resize image
-#                 scale = min(w / iw, h / ih)
-#                 nw = int(iw * scale)
-#                 nh = int(ih * scale)
-#                 dx = (w - nw) // 2
-#                 dy = (h - nh) // 2
-#                 image_data1 = 0
-#                 if proc_img:
-#                     image = image.resize((nw, nh), Image.BICUBIC)
-#                     new_image = Image.new('RGB', (w, h), (128, 128, 128))
-#                     new_image.paste(image, (dx, dy))
-#                     image_data1 = np.array(new_image) / 255.
-#
-#                 # correct boxes
-#                 box_data1 = np.zeros((max_boxes, 5))
-#                 if len(box) > 0:
-#                     np.random.shuffle(box)
-#                     if len(box) > max_boxes: box = box[:max_boxes]
-#                     box[:, [0, 2]] = box[:, [0, 2]] * scale + dx
-#                     box[:, [1, 3]] = box[:, [1, 3]] * scale + dy
-#                     box_data1[:len(box)] = box
-#                 # import matplotlib.pyplot as plt
-#                 # plt.imshow(image_data)
-#                 # plt.show()
-#                 image_data.append(np.array(image_data1))
-#                 box_data.append(box_data1)
-#
-#             else:
-#
-#                 # resize image
-#                 new_ar = w/h * rand(1-jitter,1+jitter)/rand(1-jitter,1+jitter)
-#                 scale = rand(.25, 2)
-#                 if new_ar < 1:
-#                     nh = int(scale*h)
-#                     nw = int(nh*new_ar)
-#                 else:
-#                     nw = int(scale*w)
-#                     nh = int(nw/new_ar)
-#                 image = image.resize((nw,nh), Image.BICUBIC)
-#                 # plt.imshow(np.array(image))
-#                 # plt.show()
-#                 # place image
-#                 dx = int(rand(0, w-nw))
-#                 dy = int(rand(0, h-nh))
-#                 new_image = Image.new('RGB', (w,h), (128,128,128))
-#                 new_image.paste(image, (dx, dy))
-#                 image = new_image
-#                 # plt.imshow(np.array(new_image))
-#                 # plt.show()
-#
-#                 # flip image or not
-#                 flip = rand()<.5
-#                 if flip: image = image.transpose(Image.FLIP_LEFT_RIGHT)
-#
-#                 # distort image
-#                 hue = rand(-hue, hue)
-#                 sat = rand(1, sat) if rand()<.5 else 1/rand(1, sat)
-#                 val = rand(1, val) if rand()<.5 else 1/rand(1, val)
-#                 x = rgb_to_hsv(np.array(image)/255.)
-#                 x[..., 0] += hue
-#                 x[..., 0][x[..., 0]>1] -= 1
-#                 x[..., 0][x[..., 0]<0] += 1
-#                 x[..., 1] *= sat
-#                 x[..., 2] *= val
-#                 x[x>1] = 1
-#                 x[x<0] = 0
-#                 image_data1 = hsv_to_rgb(x) # numpy array, 0 to 1
-#
-#                 # correct boxes
-#                 box_data1 = np.zeros((max_boxes,5))
-#                 if len(box)>0:
-#                     np.random.shuffle(box)
-#                     box[:, [0,2]] = box[:, [0,2]]*nw/iw + dx
-#                     box[:, [1,3]] = box[:, [1,3]]*nh/ih + dy
-#                     if flip: box[:, [0,2]] = w - box[:, [2,0]]
-#                     box[:, 0:2][box[:, 0:2]<0] = 0
-#                     box[:, 2][box[:, 2]>w] = w
-#                     box[:, 3][box[:, 3]>h] = h
-#                     box_w = box[:, 2] - box[:, 0]
-#                     box_h = box[:, 3] - box[:, 1]
-#                     box = box[np.logical_and(box_w>1, box_h>1)] # discard invalid box
-#                     if len(box)>max_boxes: box = box[:max_boxes]
-#                     box_data1[:len(box)] = box
-#                 image_data.append(np.array(image_data1))
-#                 box_data.append(box_data1)
-#             # plt.imshow(image_data1)
-#             # plt.show()
-#     image_shape = np.array(image_shape)
-#     image_data = np.array(image_data)
-#     box_data = (np.array(box_data))
-#
-#     y_true = preprocess_true_boxes(box_data, input_shape[0], anchors, num_classes)  # TODO
-#     # np.savez(data_path, image_data=image_data, box_data=box_data, image_shape=image_shape)
-#     np.savez(data_path, image_data=image_data, box_data=box_data, image_shape=image_shape, y_true0=y_true[0], y_true1=y_true[1], y_true2=y_true[2])
-#     # np.savez(data_path, image_data=image_data, box_data=box_data, image_shape=image_shape, y_true=y_true)
-#     print('Saving training data into ' + data_path)
-#     # return image_data, box_data, image_shape
-#
-#     return image_data, box_data, image_shape, y_true
 
 def get_training_data(annotation_path, data_path, input_shape, anchors, num_classes, max_boxes=100, load_previous=True):
     """
@@ -183,8 +50,6 @@ def get_training_data(annotation_path, data_path, input_shape, anchors, num_clas
                 filename = filename[:-1]
             image = Image.open(filename)
             # For the case 2
-
-
             boxed_image, shape_image = letterbox_image(image, tuple(reversed(input_shape)))
             # for the case 1
             # boxed_image, shape_image = resize_image(image, tuple(reversed(input_shape)))
@@ -204,21 +69,19 @@ def get_training_data(annotation_path, data_path, input_shape, anchors, num_clas
                 input_size = np.array(input_shape[::-1])
                 # for case 2
                 new_size = (image_size * np.min(input_size/image_size)).astype('int32')
+                # Correct BB to new image
                 boxes[i:i+1, 0:2] = (boxes[i:i+1, 0:2]*new_size/image_size + (input_size-new_size)/2).astype('int32')
-                # boxes[i:i + 1, 0:2] = (boxes[i:i + 1, 0:2] * new_size / image_size).astype('int32')
                 boxes[i:i+1, 2:4] = (boxes[i:i+1, 2:4]*new_size/image_size + (input_size-new_size)/2).astype('int32')
-                # boxes[i:i + 1, 2:4] = (boxes[i:i + 1, 2:4] * new_size / image_size).astype('int32')
                 # for case 1
-                # boxes[:i + 1, 0] = (boxes[:i + 1, 0] * input_size[0] / image_size[0]).astype('int32')
-                # boxes[:i + 1, 1] = (boxes[:i + 1, 1] * input_size[1] / image_size[1]).astype('int32')
-                # boxes[:i + 1, 2] = (boxes[:i + 1, 2] * input_size[0] / image_size[0]).astype('int32')
-                # boxes[:i + 1, 3] = (boxes[:i + 1, 3] * input_size[1] / image_size[1]).astype('int32')
+                # boxes[i:i + 1, 0] = (boxes[i:i + 1, 0] * input_size[0] / image_size[0]).astype('int32')
+                # boxes[i:i + 1, 1] = (boxes[i:i + 1, 1] * input_size[1] / image_size[1]).astype('int32')
+                # boxes[i:i + 1, 2] = (boxes[i:i + 1, 2] * input_size[0] / image_size[0]).astype('int32')
+                # boxes[i:i + 1, 3] = (boxes[i:i + 1, 3] * input_size[1] / image_size[1]).astype('int32')
             box_data.append(boxes)
-    # TODO Fonctions de test
     image_shape = np.array(image_shape)
     image_data = np.array(image_data)
     box_data = (np.array(box_data))
-    y_true = preprocess_true_boxes(box_data, input_shape[0], anchors, num_classes)  # TODO
+    y_true = preprocess_true_boxes(box_data, input_shape[0], anchors, num_classes)
     # np.savez(data_path, image_data=image_data, box_data=box_data, image_shape=image_shape)
     np.savez(data_path, image_data=image_data, box_data=box_data, image_shape=image_shape, y_true0=y_true[0], y_true1=y_true[1], y_true2=y_true[2])
     # np.savez(data_path, image_data=image_data, box_data=box_data, image_shape=image_shape, y_true=y_true)
@@ -261,7 +124,7 @@ def preprocess_true_boxes(true_boxes, Input_shape, anchors, num_classes):
     boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2  # [m, T, 2]  (x, y)center point of BB
     boxes_wh = true_boxes[..., 2:4] - true_boxes[..., 0:2]  # w = x_max - x_min  [m, T, 2]
                                                             # h = y_max - y_min
-    true_boxes[..., 0:2] = boxes_xy / input_shape[::-1]  # hw -> wh TODO normalize,ki la o input shape
+    true_boxes[..., 0:2] = boxes_xy / input_shape[::-1]  # hw -> wh
     true_boxes[..., 2:4] = boxes_wh / input_shape[::-1]  # hw -> wh
 
     N = true_boxes.shape[0]
@@ -341,6 +204,7 @@ def preprocess_true_boxes(true_boxes, Input_shape, anchors, num_classes):
 
 
 # def generate_colors(class_names):
+#     import colorsys
 #     hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
 #     colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
 #     colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
@@ -352,6 +216,7 @@ def preprocess_true_boxes(true_boxes, Input_shape, anchors, num_classes):
 #
 # def scale_boxes(boxes, image_shape):
 #     """ Scales the predicted boxes in order to be drawable on the image"""
+#     import tensorflow as tf
 #     height = image_shape[0]
 #     width = image_shape[1]
 #     image_dims = tf.constant([height, width, height, width])
